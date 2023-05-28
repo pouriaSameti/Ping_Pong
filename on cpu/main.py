0312d61b6f014dd61e2286cdc334bd94c0f11d86
pouria <pouria.sameti80@gmail.com>
2023-05-28 13:11:51 +0330

    main -> openGL & pygame initialization

--- on cpu/main.py
+++ on cpu/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-# Press the green button in the gutter to run the script.
+def init_orthographic():  # this function sets the orthographic view for viewer
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, 640, 0, 480)  # this function sets the range of the execution panel
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.init()  # initialization for the execution window by using pygame engine
+
+    width = 1000
+    height = 800
+
+    screen = pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
+    pygame.display.set_caption("ping pong")
+
+    init_orthographic()
+
+    done = False
+    while not done:
+        for event in pygame.event.get():  # this loop checks event queue every 100 millisecond's
+            if event.type == pygame.QUIT:
+                done = True
+
+            glClear(GL_COLOR_BUFFER_BIT)  # This function clears buffers to preset values
+            glMatrixMode(GL_MODELVIEW)
+            glLoadIdentity()
+
+        pygame.display.flip()  # this functions from pygame checks frame buffer and flush it every 100 millisecond's
+
+    pygame.quit()
+
+
